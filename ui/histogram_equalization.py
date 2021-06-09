c1f6f58a028a1c7543fbecd5cca7ba7fce3df32f
Siddharth Shekhar <30648642+Sid0518@users.noreply.github.com>
2021-06-09 13:27:35 +0530

    Added show_video() to histogram_equalization.py and refactored

--- ui/histogram_equalization.py
+++ ui/histogram_equalization.py
@@ -1,17 +1,31 @@
 import cv2
 import streamlit as st
 
+from .utils import display_LR, display_LR_video, get_video_feed
+
+
 def show_image(image):
   if image is not None:
     st.header("Original")
     st.image(image)
-
-    c1, c2 = st.beta_columns(2)
-
+    
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    c1.header("Grayscale")
-    c1.image(gray)
-
     result = cv2.equalizeHist(gray)
-    c2.header("Result")
-    c2.image(result)
+    display_LR(gray, result, h1= "Grayscale")
+
+
+def show_video(video):
+  if video is not None:
+    st.header("Original")
+    p = st.empty()
+    p1, p2 = display_LR_video(h1= "Grayscale")
+
+    while True:
+      video_feed = get_video_feed(video)
+      for frame in video_feed:
+        p.image(frame)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        result = cv2.equalizeHist(gray)
+        p1.image(gray)
+        p2.image(result)
