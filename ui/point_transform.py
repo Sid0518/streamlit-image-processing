6969d21cb6cf572200ea954733873e784cca287d
Siddharth Shekhar <30648642+Sid0518@users.noreply.github.com>
2021-06-09 13:26:53 +0530

    Added show_video() to point_transform.py and refactored

--- ui/point_transform.py
+++ ui/point_transform.py
@@ -1,6 +1,8 @@
 import cv2
 import streamlit as st
 
+from .utils import display_LR, display_LR_video, get_video_feed
+
 def grayscale(image):
   return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -29,6 +31,7 @@ def point_transform_menu(container):
   option = container.radio("Transform", options)
   return option
 
+
 def get_result(image, transform):
   if transform == "Grayscale":
     result = grayscale(image)
@@ -39,17 +42,23 @@ def get_result(image, transform):
 
   return result
 
+
 def show_image(image):
   transform = point_transform_menu(st)
 
   if image is not None:
-    c1, c2 = st.beta_columns(2)
-    c1.header("Original")
-    c1.image(image)
-
     result = get_result(image, transform)
-    c2.header("Result")
-    c2.image(result)
+    display_LR(image, result)
 
-def show_video(video, video_type):
+
+def show_video(video):
   transform = point_transform_menu(st)
+  if video is not None:
+    p1, p2 = display_LR_video()
+
+    while True:
+      video_feed = get_video_feed(video)
+      for frame in video_feed:
+        result = get_result(frame, transform)
+        p1.image(frame)
+        p2.image(result)
