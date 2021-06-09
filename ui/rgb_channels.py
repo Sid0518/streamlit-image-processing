d7cc136b656f3dd43e6644a3e4edd54b7bf1f8d4
Siddharth Shekhar <30648642+Sid0518@users.noreply.github.com>
2021-06-09 13:27:13 +0530

    Added show_video() to rgb_channels.py and refactored

--- ui/rgb_channels.py
+++ ui/rgb_channels.py
@@ -1,5 +1,6 @@
 import numpy as np
 import streamlit as st
+from .utils import get_video_feed
 
 def _red(image):
   out = np.zeros_like(image)
@@ -28,3 +29,26 @@ def show_image(image):
     c2.image(_green(image))
     c3.header("Blue")
     c3.image(_blue(image))
+
+  
+def show_video(video):
+  if video is not None:
+    st.header("Original")
+    p = st.empty()
+    
+    c1, c2, c3 = st.beta_columns(3)
+    c1.header("Red")
+    p1 = c1.empty()
+    c2.header("Green")
+    p2 = c2.empty()
+    c3.header("Blue")
+    p3 = c3.empty()
+
+    while True:
+      video_feed = get_video_feed(video)
+      
+      for frame in video_feed:
+        p.image(frame)
+        p1.image(_red(frame))
+        p2.image(_green(frame))
+        p3.image(_blue(frame))
